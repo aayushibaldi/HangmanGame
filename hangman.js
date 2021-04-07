7a7fda2568f771efc391dc84b872ac6cf3b3d971
Aayushi Baldi <aayushinbaldi@gmail.com>
2021-04-07 21:55:59 +0530

    Another Update

--- hangman.js
+++ hangman.js
@@ -59,7 +59,7 @@ class Hangman {
         }
 
         if (isUnique) {
-            this.guessedLetters.push(guess)
+            this.guessedLetters = [...this.guessedLetters, guess]
         }
 
         if (isUnique && isBadGuess) {
